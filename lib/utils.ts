106b9affbf1e45d2678bb7967fc0e57248d9980c
kunal-shetty <kunalshetty567@gmail.com>
2026-08-31 22:14:00 +0530

    chore(utils): refine shared utilities and document helpers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
+/**
+ * @file utils.ts
+ * @description General purpose utility functions used throughout the application.
+ */
+
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Merges Tailwind CSS classes without conflicts.
+ * Combines `clsx` for conditional classes and `twMerge` for priority resolution.
+ * @param inputs A list of class values (strings, arrays, or objects).
+ * @returns A single, merged class string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
