f29c57c5d34bee5a92611eb172fba79100757d52
kunal-shetty <kunalshetty567@gmail.com>
2026-09-05 14:42:44 +0530

    feat(home): refine app entry experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-import { EchoApp } from "@/components/echo-shell";
+"use client";
+
+import { HomeScreen } from "@/components/home/home-screen";
+import { useTransactions } from "@/lib/use-transactions";
+import { useApp } from "@/context/AppContext";
 
 export default function Page() {
-  return <EchoApp />;
+  const { user } = useApp();
+  const tx = useTransactions();
+
+  return (
+    <HomeScreen
+      setScreen={() => {}} // No longer used for routing
+      onVoice={() => {}} // Handled by AppShell
+      expenses={tx.transactions}
+      loading={tx.loading}
+      configured={tx.configured}
+      scrolled={false} // Handled by AppShell
+      user={user}
+      onRowClick={() => {}} // This should be a link to a detail page if we had one
+    />
+  );
 }
+
